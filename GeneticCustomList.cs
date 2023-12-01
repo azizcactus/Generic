8b16347e4edf63f406a496a79ac537eac3af3e28
azizcactus <148267478+azizcactus@users.noreply.github.com>
2023-12-01 19:10:53 +0400

    Add files via upload

---
+++ GeneticCustomList.cs
@@ -0,0 +1,132 @@
+﻿using System.Drawing;
+
+namespace Pain
+{
+    public class GeneticCustomList<T>
+    {
+        private List<T> elements;
+        private int count;
+        private int capacity=0; 
+
+        
+        public GeneticCustomList()
+        {
+            elements = new List<T>();
+            count = 0;
+           
+        }
+        public GeneticCustomList( int _capacity)
+        {
+            elements = new List<T>();
+            count = 0;
+            capacity = _capacity;
+        }
+
+        public int Count 
+        {
+            get { return count; }
+        }
+        public int Capacity
+        {
+            get { return capacity; }
+        } 
+       
+        public void Add(T item)
+        {
+            elements.Add(item);
+
+            if (elements.Count >= capacity)
+            {
+                if (capacity == 0)
+                {
+                    capacity = 4;
+                }
+                else
+                {
+                    capacity *= 2;
+                }
+            }
+            elements[count] = item;
+            count++;
+        }
+        public T Find (Predicate<T> match)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (match(elements[i]))
+                {
+                    return elements[i];
+                }
+            }
+            return default(T); 
+         
+        }
+        public List<T> FindAll(Predicate<T> match)
+        {
+            List<T> resultList = new List<T>();
+            for (int i = 0; i < count; i++)
+            {
+                if (match(elements[i]))
+                {
+                    resultList.Add(elements[i]);
+                }
+            }
+            return resultList;
+        }
+        public bool Contains (T item)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(elements[i], item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool Exists(Predicate<T> match)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (match(elements[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        
+    }
+        public bool Remove(T item)
+        {
+            int indexToRemove = -1;
+
+            
+            for (int i = 0; i < count; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(elements[i], item))
+                {
+                    indexToRemove = i;
+                    break;
+                }
+            }
+
+            
+            if (indexToRemove == -1)
+                return false;
+
+            
+            for (int i = indexToRemove; i < count - 1; i++)
+            {
+                elements[i] = elements[i + 1];
+            }
+
+            
+            count--;
+
+            
+            return true;
+        }
+    }
+}
+            
+       
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Pain;
+GeneticCustomList<int> generic = new GeneticCustomList<int>();
+
+generic.Add(9);
+generic.Add(8);
+generic.Add(7);
+generic.Add(6);
+generic.Add(5);
+generic.Add(4);
+
+generic.Remove(4);
+
+//************CONSOLE******************
+foreach (int i in generic.FindAll(num => num < 10))
+{
+    Console.WriteLine(i);
+}
+
+//Console.WriteLine(generic.Find(num => num < 10));
+//Console.WriteLine(generic.Find(num => num > 10));
+//Console.WriteLine(generic.Contains(10));
+//Console.WriteLine(generic.Exists(num => num > 1));
+//Console.WriteLine(generic.Count);
+//Console.WriteLine(generic.Capacity);
